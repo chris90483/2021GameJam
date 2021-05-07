3799bceb59d02912f9c6cf29f8d4ae5a4c13f3ff
chris90483 <chris9898@live.nl>
2021-05-07 19:26:06 +0200

    Boilerplate code

---
+++ main/main.py
@@ -0,0 +1,74 @@
+import pygame
+import sys
+import time
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FRAME_RATE = 60
+offset = 0
+
+# setup stuff
+pygame.init()
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+pygame.font.init()
+font = pygame.font.SysFont("Arial", 20)
+msg_surface = None
+
+
+# Do all necessary setup
+def setup():
+    global msg_surface
+    msg_surface = font.render("it works!", False, (255, 255, 255))
+
+
+
+# handle a pressed key event in the context of the game root
+def handle_key_press(event_key):
+    if event_key == pygame.K_ESCAPE:
+        # end the program, close the window
+        pygame.quit()
+        sys.exit()
+
+
+# Handle all pygame events
+def handle_events():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                handle_key_press(event.key)
+
+
+# Do all updates to the game state in this function
+def update_state():
+    global offset, window
+    window.fill((0, 0, 0))
+
+    object_width = msg_surface.get_width()
+    o1 = (offset + 1) % SCREEN_WIDTH
+    o2 = (offset + 1 + object_width) % SCREEN_WIDTH
+    offset = o1
+    if o2 != (o1 + msg_surface.get_width()):
+        print(o2-o1 - object_width)
+        window.blit(msg_surface, (o2 - object_width, 0))
+
+    window.blit(msg_surface, (offset, 0))
+
+
+if __name__ == '__main__':
+    setup()
+    while True:
+        start_time = time.clock()
+
+        # handle pygame events from the queue
+        handle_events()
+        # update the state of the game
+        update_state()
+
+        # possibly delay program execution to ensure steady frame rate
+        running_time = time.clock() - start_time
+        if running_time < 1/FRAME_RATE:
+            time.sleep((1/FRAME_RATE) - running_time)
+
+        pygame.display.update()
