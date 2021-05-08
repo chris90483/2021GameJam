fa7ed92a2612623c043d6328eb53339e8babf8e8
wilco375 <wilcovanbeijnum@gmail.com>
2021-05-08 19:59:03 +0200

    Add scoring

--- entities/delivery_status.py
+++ entities/delivery_status.py
@@ -1,16 +1,26 @@
 import pygame
-from pygame.event import EventType
+
 from main.constants import Constant
-from math import atan2, pi
 
 
 class DeliveryStatus(object):
-    def __init__(self, destination):
+    def __init__(self, destination, score):
         self.destination = destination
+        self.score = score
+        self.font = pygame.font.SysFont("Arial", 20)
 
     def draw(self, screen: pygame.Surface):
         progress = self.destination.get_delivery_progress()
         if progress > 0:
             progress_bar_width = 200
-            pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(50, Constant.SCREEN_HEIGHT - 75, progress_bar_width, 25))
-            pygame.draw.rect(screen, (206, 83, 72), pygame.Rect(50, Constant.SCREEN_HEIGHT - 75, progress_bar_width * progress, 25))
+            pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(50, Constant.SCREEN_HEIGHT - 100, progress_bar_width, 25))
+            pygame.draw.rect(screen, (253, 127, 0), pygame.Rect(50, Constant.SCREEN_HEIGHT - 100, progress_bar_width * progress, 25))
+
+        progress = self.destination.get_finishing_delivery_progress()
+        if progress > 0:
+            progress_bar_width = 200
+            pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(50, Constant.SCREEN_HEIGHT - 150, progress_bar_width, 25))
+            pygame.draw.rect(screen, (206, 83, 72), pygame.Rect(50, Constant.SCREEN_HEIGHT - 150, progress_bar_width * progress, 25))
+
+        text_surface = self.font.render('Score: {}'.format(self.score.get_score()), True, (255, 255, 255))
+        screen.blit(text_surface, (50, Constant.SCREEN_HEIGHT - text_surface.get_height() // 2 - 50))
--- main/destination.py
+++ main/destination.py
@@ -1,39 +1,47 @@
 from random import choice
 from math import hypot
 from main.grid import CellType
-import time
 from main.constants import Constant
 
 class Destination:
     destination = None
     destination_doominos = False  # Is the current destination to the Doomino's
-    delivering = None
+    delivery_time = 0  # Time that the delivery started in general
+    finishing_delivery_time = None  # Time that the delivery started at the location
 
     total_deliveries = 0  # Total number of deliveries that the player has made so far
 
-    def __init__(self, grid, player):
+    steps = 0
+
+    def __init__(self, grid, player, score):
         self.grid = grid
         self.player = player
+        self.score = score
 
         self.__generate_destination()
 
     def step(self):
         if self.__player_at_delivery_location():
-            if self.delivering is None:
-                # Set the current time as the delivery start time and wait for Constant.DELIVERY_TIME
-                self.delivering = time.time()
-            elif time.time() - self.delivering >= Constant.DELIVERY_TIME:
+            if self.finishing_delivery_time is None:
+                # Set the current time as the delivery start time and wait for Constant.DELIVERY_FINISHING_TIME seconds
+                self.finishing_delivery_time = self.steps
+            elif self.steps - self.finishing_delivery_time >= Constant.DELIVERY_FINISHING_TIME * Constant.FRAME_RATE:
                 # Update the delivery destination
-                self.delivering = None
+                self.finishing_delivery_time = None
                 if not self.destination_doominos:
                     self.destination = self.grid.doominos_location
                     self.destination_doominos = True
                     self.total_deliveries += 1
+                    self.score.increment_score(int(self.get_delivery_progress() * Constant.DELIVERY_SCORE_INCREASE))
+                    self.delivery_time = None
                 else:
                     self.__generate_destination()
+                    self.delivery_time = self.steps
                     self.destination_doominos = False
         else:
-            self.delivering = None
+            self.finishing_delivery_time = None
+
+        self.steps += 1
 
     def __player_at_delivery_location(self):
         """
@@ -60,10 +68,20 @@ class Destination:
 
     def get_delivery_progress(self):
         """
+        Return the time left for the delivery on a scale from 1 (full time left) to 0 (no time left)
+        :return: delivery progress
+        """
+        if self.delivery_time is None:
+            return 0
+        else:
+            return 1 - min(1, (self.steps - self.delivery_time) / Constant.DELIVERY_TIME / Constant.FRAME_RATE)
+
+    def get_finishing_delivery_progress(self):
+        """
         Return the progress of the delivery on a scale from 0 (not delivered) to 1 (delivered)
         :return: delivery progress
         """
-        if self.delivering is None:
+        if self.finishing_delivery_time is None:
             return 0
         else:
-            return min(1, (time.time() - self.delivering) / Constant.DELIVERY_TIME)
+            return min(1, (self.steps - self.finishing_delivery_time) / Constant.DELIVERY_FINISHING_TIME / Constant.FRAME_RATE)
