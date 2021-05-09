365dedd39a632571121aac673715859b00e78118
gerryflap <gerry2211@gmail.com>
2021-05-09 17:32:45 +0200

    Fixed the emitter again

--- audio/emitter_handler.py
+++ audio/emitter_handler.py
@@ -56,7 +56,7 @@ class EmitterHandler(object):
         for emitter in self.current_emitters:
             self.emitter_surface.fill((0, 0, 0, 0))
             emitter.draw(self.emitter_surface, camera)
-        screen.blit(self.emitter_surface, (0, 0))
+            screen.blit(self.emitter_surface, (0, 0))
         self.lock.release()
 
     def __del__(self):
