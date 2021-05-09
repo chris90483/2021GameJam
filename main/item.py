a60d7ed2d94c346ba3401b324f0817c330941709
chris90483 <chris9898@live.nl>
2021-05-09 18:31:31 +0200

    Je kan nu een klokje drinken in het park

--- main/item.py
+++ main/item.py
@@ -35,6 +35,19 @@ class Pizza(Item):
         window.blit(item_texture, (x, y))
 
 
+class Klok(Item):
+    def __init__(self, item_type, inventory_icon_file_name):
+        super().__init__(item_type, inventory_icon_file_name)
+        self.item_type = item_type
+        self.inventory_icon_file_name = inventory_icon_file_name
+
+    def step(self):
+        pass
+
+    def draw_inventory_slot(self, window: Surface, camera, x, y):
+        item_texture = pygame.image.load(self.IMAGE_FOLDER_LOCATION + self.inventory_icon_file_name)
+        window.blit(item_texture, (x, y))
+
 class Knife(Item):
 
     def __init__(self, item_type, inventory_icon_file_name, player):
