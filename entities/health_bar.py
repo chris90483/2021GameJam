538f320b793276dbffc2a378443967db3895b7e6
wilco375 <wilcovanbeijnum@gmail.com>
2021-05-09 17:14:13 +0200

    Fix health bar

--- entities/health_bar.py
+++ entities/health_bar.py
@@ -18,7 +18,7 @@ class HealthBar(object):
                                      health_bar_width, health_bar_height))
         pygame.draw.rect(screen, (0, 128, 0),
                          pygame.Rect(Constant.SCREEN_WIDTH - (50 + health_bar_width), Constant.SCREEN_HEIGHT - (75 + health_bar_height),
-                                     health_bar_width * (self.player.health / 1000.0), health_bar_height))
+                                     health_bar_width * (self.player.health / Constant.PLAYER_HEALTH), health_bar_height))
 
         text_surface = self.font.render('Health: {}'.format(self.player.health), True, (255, 255, 255))
         screen.blit(text_surface, (Constant.SCREEN_WIDTH - (50 + health_bar_width), Constant.SCREEN_HEIGHT - 60))
