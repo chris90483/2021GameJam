e9a22ee33c0945b6add15deaf3a5c268f59370db
wilco375 <wilcovanbeijnum@gmail.com>
2021-05-09 17:44:13 +0200

    Slow down skateboard on grass

--- entities/player.py
+++ entities/player.py
@@ -226,16 +226,14 @@ class Player(object):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         self.angle = atan2(- (Constant.SCREEN_HEIGHT // 2 - mouse_y), Constant.SCREEN_WIDTH // 2 - mouse_x)
 
+        speed = Constant.PLAYER_SPEED
         if self.world.inventory.items[self.world.inventory.current_item]:
             if self.world.inventory.items[self.world.inventory.current_item].item_type == InventoryItem.SKATEBOARD:
-              speed = Constant.PLAYER_SPEED_SKATEBOARD
-            else:
-                speed = Constant.PLAYER_SPEED
-        else:
-            speed = Constant.PLAYER_SPEED
+                speed = Constant.PLAYER_SPEED_SKATEBOARD
         player_pos = self.get_grid_position()
         if self.grid.grid[player_pos[0]][player_pos[1]].type == CellType.NATURE:
-            speed *= Constant.PLAYER_SPEED_GRASS_MULTIPLIER
+            # If we are on grass we are always slow, also with a skateboard
+            speed = Constant.PLAYER_SPEED * Constant.PLAYER_SPEED_GRASS_MULTIPLIER
 
         if self.held_keys[pygame.K_LSHIFT]:
             speed *= Constant.PLAYER_SPEED_SLOW_WALKING_MULTIPLIER
